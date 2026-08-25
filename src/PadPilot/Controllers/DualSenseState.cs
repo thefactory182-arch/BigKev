70e0952ecd99d7834d821d78e815346772f44ff3
thefactory182-arch <thefactory182@gmail.com>
2026-08-25 11:44:25 -0400

    Add src/PadPilot/Controllers/DualSenseState.cs

---
+++ src/PadPilot/Controllers/DualSenseState.cs
@@ -0,0 +1,12 @@
+namespace PadPilot.Controllers;
+
+public sealed record DualSenseState(
+    byte LeftX, byte LeftY, byte RightX, byte RightY,
+    byte LeftTrigger, byte RightTrigger,
+    IReadOnlySet<string> Buttons)
+{
+    public string Summary => Buttons.Count == 0
+        ? $"Sticks L {LeftX},{LeftY}  R {RightX},{RightY}"
+        : string.Join(" + ", Buttons);
+}
+
